32cf52bb43f574167dbb1a4d538ad18ccff78274
Nitya <gorenityanand96@gmail.com>
2022-01-30 23:01:51 +0530

    PartTimeEmp wage calculation

--- WageComputation.cs
+++ WageComputation.cs
@@ -5,21 +5,32 @@ namespace EmployeeWageComputation
     public class WageComputation
     {
         
-        const int EMP_PRESENT = 0, EMP_ABSENT = 1, EMP_WAGE_PER_HR = 20, FULL_DAY = 8; 
+        const int EMP_PRESENT = 0, EMP_ABSENT = 1, EMP_WAGE_PER_HR = 20, FULL_DAY = 8 , PART_TIME = 4; 
+        int dayhr= 0, dailyWage=0;
 
         Random random = new Random();
-
         
         public void employeeAttendence()
         {
+
             int empCheck = random.Next(0, 2);
-            
 
             if (empCheck == EMP_PRESENT)
             {
                 Console.WriteLine("Employee is Present..");
-                int dailyWage = EMP_WAGE_PER_HR * FULL_DAY;
-                Console.WriteLine("Daily Wage is :"+dailyWage);
+
+                if (dayhr == FULL_DAY)
+                {
+                    dailyWage = EMP_WAGE_PER_HR * FULL_DAY;
+                    
+                }
+                else
+                {
+                    dailyWage = EMP_WAGE_PER_HR * PART_TIME;
+                    
+                }
+                
+                Console.WriteLine("Daily Wage is :"+dailyWage);    
             }
             if (empCheck == EMP_ABSENT)
             {
