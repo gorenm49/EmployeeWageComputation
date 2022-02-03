9565747d8cdf4581aeb36a64d99382a112b13b50
Nitya <gorenityanand96@gmail.com>
2022-02-03 15:57:08 +0530

    [Nityanand]:Wage till total working hr per month Added

--- WageComputation.cs
+++ WageComputation.cs
@@ -5,36 +5,43 @@ namespace EmployeeWageComputation
     public class WageComputation
     {
         
-        const int EMP_WAGE_PER_HR = 20, FULL_DAY = 8, PART_TIME = 4, F_DAY= 1, H_DAY = 0 , WORK_DAY_IN_MONTH =20;
+        const int EMP_WAGE_PER_HR = 20, FULL_DAY = 8, PART_TIME = 4, F_DAY= 1, H_DAY = 0 , WORK_DAY_IN_MONTH =20, TOTAL_WORK_HR_FOR_MONTH = 100;
 
         Random random = new Random();
 
-        int empHr = 0, empWageForDay= 0, totalEmpWageForMonth = 0;
+        int empHr = 0, totalEmpHr= 0, totalWorkingDays = 0;
 
         public void CalculateWage()
         {
-            int empCheck1 = random.Next(0, 3);
-            
-            switch (empCheck1)
+            while (totalEmpHr <= TOTAL_WORK_HR_FOR_MONTH && totalWorkingDays < WORK_DAY_IN_MONTH )
             {
-                case F_DAY: 
-                    Console.WriteLine("Employee is Present..");
-                    this.empHr += FULL_DAY;
-                    break;
-                case H_DAY: 
-                    Console.WriteLine("Employee is Present..");
-                    this.empHr += PART_TIME;
-                    break;
-                default:
-                    Console.WriteLine("Employee is absent..");
-                    this.empHr += 0;
-                    break;
+                totalWorkingDays++;
+
+            
+                int empCheck1 = random.Next(0, 3);
+            
+                switch (empCheck1)
+                {
+                    case F_DAY: 
+                        Console.WriteLine("Employee is Present..");
+                        this.empHr += FULL_DAY;
+                        break;
+
+                    case H_DAY: 
+                        Console.WriteLine("Employee is Present..");
+                        this.empHr += PART_TIME;
+                        break;
+
+                    default:
+                        Console.WriteLine("Employee is absent..");
+                        this.empHr += 0;
+                        break;
+                }
+                totalEmpHr +=empHr;
+                System.Console.WriteLine("Total Working Days Of EMP: "+totalWorkingDays+"\n Working Hrs :"+totalEmpHr);
             }
 
-            this.empWageForDay = empHr * EMP_WAGE_PER_HR;
-            Console.WriteLine(this.empWageForDay);
-
-            totalEmpWageForMonth = empWageForDay * WORK_DAY_IN_MONTH;
+            int totalEmpWageForMonth = totalEmpHr * WORK_DAY_IN_MONTH;
             System.Console.WriteLine("Monthly Wage for Employee is :"+totalEmpWageForMonth);    
         }    
     }
