0135daa10f051b12e7f107642550252411c3e83e
Nitya <gorenityanand96@gmail.com>
2022-02-06 14:42:14 +0530

    [Nityanand]:Minor chnages :added

--- EmpWageBuilder.cs
+++ EmpWageBuilder.cs
@@ -4,53 +4,31 @@ namespace EmployeeWageComputation
 {
     public class EmpWageBuilder
     {
-        const int FULL_DAY = 8, PART_TIME = 4,F_DAY= 1, H_DAY = 0;
+        
+        const int  FULL_DAY = 8, PART_TIME = 4, F_DAY= 1, H_DAY = 0;
 
-        private int countOfCompany = 0;
-        private CompanyEmpWage[] companyEmpWageArray;
+        Random random = new Random();
 
-        public EmpWageBuilder()
+        int empHr = 0, totalEmpHr= 0, totalWorkingDays = 0;
+
+        public void CalculateWage(string company, int empWagePerHr, int workDaysInMonth, int totalWorkHrForMonth)
         {
-            this.companyEmpWageArray = new CompanyEmpWage[5];
-        }
-
-        public void addCompanyWage(string company, int empWagePerHr, int workDaysInMonth, int totalWorkHrForMonth)
-        {
-            companyEmpWageArray[this.countOfCompany] = new CompanyEmpWage(company, empWagePerHr, workDaysInMonth, totalWorkHrForMonth);
-            countOfCompany++;
-        }
-
-        public void CalculateWage()
-        {
-            for (int i = 0; i < countOfCompany; i++)
-            {
-                companyEmpWageArray[i].setTotalEmpWage(this.CalculateWage(this.companyEmpWageArray[i]));
-                System.Console.WriteLine(this.companyEmpWageArray[i].dislpay());
-            }
-        }
-
-        private void CalculateWage(CompanyEmpWage c1)
-        {
-
-            int empHr = 0, totalEmpHr= 0, totalWorkingDays = 0;
-
-            while (totalEmpHr <= c1.totalWorkHrForMonth && totalWorkingDays <= c1.workDaysInMonth )
+            while (totalEmpHr <= totalWorkHrForMonth && totalWorkingDays <= workDaysInMonth )
             {
                 totalWorkingDays++;
-
-                Random random = new Random();
+ 
                 int empCheck1 = random.Next(0, 3);
             
                 switch (empCheck1)
                 {
                     case F_DAY: 
                         Console.WriteLine("Employee is Present..");
-                        this.empHr += this.FULL_DAY;
+                        this.empHr += FULL_DAY;
                         break;
 
                     case H_DAY: 
                         Console.WriteLine("Employee is Present..");
-                        this.empHr += this.PART_TIME;
+                        this.empHr += PART_TIME;
                         break;
 
                     default:
@@ -59,12 +37,12 @@ namespace EmployeeWageComputation
                         break;
                 }
                 totalEmpHr +=empHr;
-                System.Console.WriteLine("Total Working Days Of EMP: "+this.totalWorkingDays+"\n Working Hrs in month is :"+this.empHr);
+                System.Console.WriteLine("Total Working Days Of EMP: "+totalWorkingDays+"\n Working Hrs in month is :"+empHr);
             }
 
-            return totalEmpHr * c1.workDaysInMonth;
-            
-        }
-
+            int totalEmpWageForMonth = totalEmpHr * workDaysInMonth;
+            System.Console.WriteLine("The wage by Company "+company+",  for the employee is : "+totalEmpWageForMonth);
+        }    
     }
+    
 }
